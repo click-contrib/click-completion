55177aa975ce2adb270d911fc5392a99cc718ae2
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2018-04-06 13:55:41 +0200

    bump version to 0.3.1

--- click_completion/__init__.py
+++ click_completion/__init__.py
@@ -15,7 +15,7 @@ import six
 
 from click import echo, MultiCommand, Option, Argument, ParamType
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 _invalid_ident_char_re = re.compile(r'[^a-zA-Z0-9_]')
 
