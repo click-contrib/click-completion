af2d5495ef2946a6d0b07461bc98600494143c2f
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2019-10-15 18:18:18 +0200

    fix slow completion on projects that provide a cached get_command_short_help
    the method was not used anymore. Also add get_command_hidden that may also be cached

--- click_completion/core.py
+++ click_completion/core.py
@@ -127,9 +127,8 @@ def get_choices(cli, prog_name, args, incomplete):
                         choices.append((opt, None))
         if isinstance(ctx.command, MultiCommand):
             for name in ctx.command.list_commands(ctx):
-                command = ctx.command.get_command(ctx, name)
-                if match(name, incomplete) and not command.hidden:
-                    choices.append((name, command.get_short_help_str()))
+                if match(name, incomplete):
+                    choices.append((name, ctx.command.get_command_short_help(ctx, name)))
 
     for item, help in choices:
         yield (item, help)
